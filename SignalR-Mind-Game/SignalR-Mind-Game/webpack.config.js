78f1d584352c78938d2cc89fd57f6983498f5eb7
TomC333 <d.dadiani343@gmail.com>
2025-06-12 16:43:57 +0400

    chore: revert deleted webpack.config.js

---
+++ SignalR-Mind-Game/SignalR-Mind-Game/webpack.config.js
@@ -0,0 +1,62 @@
+﻿const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        auth: './src/index.ts',
+        game: './src/game.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+        filename: "[name].[chunkhash].js",
+        publicPath: "/",
+        clean: true,
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'assets',
+            },
+            {
+                test: /\.mp3$/,
+                type: 'assets/sounds',
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html",
+            chunks: ['auth'],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "game.html",
+            template: "./src/game.html",
+            chunks: ['game']
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[chunkhash].css",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "src/assets", to: "assets" },
+            ],
+        }),
+    ],
+};
